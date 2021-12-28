bdc715e28ed1405cb3fa968f54251fae2fb253de
Matt <58913953+ms5984@users.noreply.github.com>
2021-12-28 15:38:00 -0500

    Update API target * MC version 1.18.1 * Increment project to 1.0.1 * cleanup import

--- src/main/java/com/github/ms5984/survivelist/endperm/EndPerm.java
+++ src/main/java/com/github/ms5984/survivelist/endperm/EndPerm.java
@@ -38,7 +38,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.Collections;
 import java.util.LinkedList;
